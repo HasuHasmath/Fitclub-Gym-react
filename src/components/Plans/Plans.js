b71363e3d4022554584fb96bdcd81256790d5bc3
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-28 01:37:50 +0530

    plans section

--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Plans.css'
+import React from "react";
+import "./Plans.css";
 
 const Plans = () => {
   return (
     <div>
-      
+      <div className="plans-container">
+        <div className="programs-header" style={{ gap: "2rem" }}>
+          <span className="stroke-text">READY TO START</span>
+          <span>YOUR JOURNEY</span>
+          <span className="stroke-text">NOW WITHUS</span>
+        </div>
+        <div className="programs-header">
+          <span className="stroke-text">hasmath</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Plans
+export default Plans;
