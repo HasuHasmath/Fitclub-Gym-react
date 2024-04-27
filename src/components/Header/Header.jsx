5be29851e4a7346602fb4966ecc54d6aabed7068
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-27 15:47:51 +0530

    end of programs section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/logo.png"
+
 const Header = () => {
   return (
     <div>
