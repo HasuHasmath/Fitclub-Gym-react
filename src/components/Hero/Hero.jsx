6ec81d2799cbde69d3fb7e497788ae4b19624496
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-27 14:54:51 +0530

    .

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
-import hero_image from "../../assets/hero_image.png"
+import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
-import Heart from "../../assets/heart.png"
+import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
-
-
 
 const Hero = () => {
   return (
@@ -62,8 +60,22 @@ const Hero = () => {
       <div className="right-h">
         <button className="btn">Join now</button>
 
-        <div className="heart-rate"><img src={Heart} alt="" />
-        <span>Heart Rate</span><span>116 bpm</span></div>
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/* hero images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
       </div>
     </div>
   );
