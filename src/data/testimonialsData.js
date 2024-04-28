22fec201b2609e9c475bd68fb788368281250f94
hasuhasmath <hasuh5pepe@gmail.com>
2024-04-28 14:52:45 +0530

    end of testimonials

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -7,19 +7,21 @@ export const testimonialsData = [
     image: image1,
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+    name: "MATHEW HENDRICKSON",
+    status: "ENTREPRENEUR",
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    review:
+      "The gym boasts modern facilities and dedicated trainers, offering personalized fitness plans and motivating workouts. Clients appreciate the supportive atmosphere and professional guidance provided by the gym's trainers. ",
+    name: "JOHN KEVIN",
+    status: "COACH",
   },
   {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
+    image: image3,
+    review:
+      " The gym is known for its top-notch equipment and knowledgeable trainers who inspire clients to reach their fitness goals. Customers value the personalized attention and positive environment fostered by the gym's trainers.",
+    name: "FRANKLIN",
+    status: "CUSTOMER",
+  },
 ];
